3f57694fbf2fe6619087d54489ed904c3f69a5de
Dhairya <118334190+Dhairya-A-Mehra@users.noreply.github.com>
2023-08-28 10:22:13 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+main()
+{
+  //  char a;
+  //  cin>>a;
+    cout<<"Hello World\n";
+    cout<<"Hello World\n"<<endl;
+   // cout<<a;
+   cout<<"Hello";
+   cout<<"World"<<endl;
+   cout<<"Hello\t"<<"World"<<endl;
+   cout<<"Hello"<<"World\n";
+    
+
+}

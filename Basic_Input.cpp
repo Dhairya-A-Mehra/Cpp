72419fb194416385b0277a37bc3a4bbc21ed31dd
Dhairya <118334190+Dhairya-A-Mehra@users.noreply.github.com>
2023-08-28 10:22:53 +0530

    Add files via upload

---
+++ Basic_Input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a;
+    int b;
+    double c;
+    cout<<"Enter a: "<<endl;
+    cin>>a;
+    cout<<"Enter b: "<<endl;
+    cin>>b;
+    cout<<"Enter c: "<<endl;
+    cin>>c;
+    cout<<"You entered: "<< a <<endl;
+    cout<<"You entered: "<< b <<endl;
+    cout<<"You entered: "<< c <<endl;
+    cout<<"You entered "<<a<<" and "<<b; 
+    
+    
+}
